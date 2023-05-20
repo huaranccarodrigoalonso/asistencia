b128bdceb833979c018e54640032d50e7e6026d7
rhuarancca <huaranccarodrigo@gmail.com>
2023-05-20 18:26:03 -0500

    v2.0 view CRUD

--- app/src/main/java/com/example/asistencia/MainActivity.kt
+++ app/src/main/java/com/example/asistencia/MainActivity.kt
@@ -1,43 +1,71 @@
 package com.example.asistencia
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.asistencia.ui.theme.AsistenciaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AsistenciaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            CRUD()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AsistenciaTheme {
-        Greeting("Android")
+fun CRUD() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        /*Image(
+            painter = painterResource(id = R.drawable.congreso_main),
+            contentDescription = "Congress image")
+        */
+        Button(
+            onClick = {
+            //your onclick code here
+            },
+            colors = ButtonDefaults.buttonColors(
+                //backgroundColor = Color.White,
+                contentColor = Color.Red),
+            elevation =  ButtonDefaults.buttonElevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 15.dp,
+                disabledElevation = 0.dp
+            )
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.add_assistent),
+                contentDescription = "add assistent",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "Button with elevation",
+                modifier = Modifier.padding(start = 10.dp)
+            )
+        }
     }
 }
